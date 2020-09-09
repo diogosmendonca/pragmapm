99950cfa582100d8cb51f7754f1e96219d7553b9
DIOGO SILVEIRA MENDONCA <diogosmendonca@gmail.com>
2020-09-09 17:22:35 -0300

    Navegação entre componentes

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
-import {ListagemProjetos} from '../listagem_projetos/ListagemProjetos'
+import {ListagemProjetos} from '../projetos/ListagemProjetos'
+import {FormProjeto} from '../projetos/FormProjeto'
 
 function App() {
   return (
-    <ListagemProjetos/>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/projetos/novo">Novo Projeto</Link></li>
+            <li><Link to="/projetos">Projetos</Link></li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/projetos/novo"><FormProjeto /></Route>
+          <Route path="/projetos"><ListagemProjetos/></Route>
+          <Route path="/"><ListagemProjetos/></Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
