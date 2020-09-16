17d64bf9c5defc4c3c5c6d817a5a957678e83d3a
DIOGO SILVEIRA MENDONCA <diogosmendonca@gmail.com>
2020-09-16 13:12:36 -0300

    Projeto de classe para função para ser serializável

--- src/projetos/Projeto.js
+++ src/projetos/Projeto.js
@@ -1,23 +1,14 @@
 
-const defaults = {
-    id: 0,
-    nome: '',
-    unidade: '',
-    unidadeAtual: 0,
-    unidadesTotais: 0,
-    idc: 0,
-    idp: 0
-}
-export default class Projeto{
+export default function Projeto(obj){
 
-    constructor(obj){
-        this.id = obj.id || defaults.id;
-        this.nome = obj.nome || defaults.nome;
-        this.unidade = obj.unidade || defaults.unidade;
-        this.unidadeAtual = obj.unidadeAtual || defaults.unidadeAtual;
-        this.unidadesTotais = obj.unidadesTotais || defaults.unidadesTotais;
-        this.idc = obj.idc || defaults.idc;
-        this.idp = obj.idp || defaults.idp;
-    }
+    return {
+        id: obj.id || 0,
+        nome: obj.nome || '',
+        unidade: obj.unidade || '',
+        unidadeAtual: obj.unidadeAtual || 0,
+        unidadesTotais: obj.unidadesTotais || 0,
+        idc: obj.idc || 0,
+        idp: obj.idp || 0
+    };
 
 }
