119edbd2e318746143a03383848f2f0c7d756511
DIOGO SILVEIRA MENDONCA <diogosmendonca@gmail.com>
2020-09-29 11:37:39 -0300

    Corrigindo teste quebrado

--- src/app/App.test.js
+++ src/app/App.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import {store} from '../store'
+import { Provider } from 'react-redux'
 
 test('Renderiza a app', () => {
-  const { getByText } = render(<App />);
-  const labelElement = getByText(/Listagem de Projetos/i);
+  const { getByText } = render(
+    <Provider store={store}>
+    <App />
+    </Provider>
+  );
+  const labelElement = getByText(/PragmaPM/i);
   expect(labelElement).toBeInTheDocument();
 });
